1c2daa7618122f234d56203b5132f8929e412a39
Thundorix <dehaan.frederik@gmail.com>
2024-07-08 12:28:35 -0400

    Added Char Dictionary

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ def main():
     path_to_file = "books/frankenstein.txt"
     text = readtext(path_to_file)
     total_words = word_count(text)
-    print(text)
-    print(f"Total Words: {total_words}")
+    char_dict = count_char(text)
+    #print(text)
+    #print(f"Total Words: {total_words}")
+    print(char_dict)
 
 def word_count(text):
-    count = 0
     words = text.split()
     return len(words)
 
@@ -14,6 +15,13 @@ def readtext(path_to_file):
     with open(path_to_file) as f:
         return f.read()
 
-
+def count_char(text):
+    char_dict = {}
+    for ch in text.lower():
+        if ch in char_dict:
+            char_dict[ch] += 1
+        else:
+            char_dict[ch] = 1
+    return char_dict
 
 main()
