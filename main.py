4488a556ea610d8f926cd326186ed9ada65c79ea
Thundorix <dehaan.frederik@gmail.com>
2025-08-07 10:39:18 -0400

    Added a fucntion to sort dictionary of chars into a list of dictionaries

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, sort_dict
 
 def main():
     book_path = "./books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = count_words(text)
     print(f"{num_words} words found in the document")
-    print(count_characters(text))
+    #print(count_characters(text))
+    sort_dict(text)
 
 
 def get_book_text(path):
--- stats.py
+++ stats.py
@@ -12,4 +12,17 @@ def count_characters(text):
         else:
             char_dict[ch] += 1
     return char_dict
-        
+
+
+def sort_on(items):
+    return items["num"]
+
+def sort_dict(text):
+    char_dict = count_characters(text)
+    char_dict_list = []
+    for k in char_dict:
+        if k.isalpha():
+            key_dict = {"char": k, "num": char_dict[k]}
+            char_dict_list.append(key_dict)
+    char_dict_list.sort(reverse=True, key=sort_on)
+    print(char_dict_list)
