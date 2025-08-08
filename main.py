d4964cca7274d1f34f8f460b9838467267d10dd3
Thundorix <dehaan.frederik@gmail.com>
2025-08-08 11:22:39 -0400

    Added sys.argv functionaily

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import sys
 from stats import count_words, sort_dict
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = count_words(text)
     char_dict_list = sort_dict(text)
