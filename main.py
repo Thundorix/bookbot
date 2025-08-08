1c214b97c9a70292cc8b53e32ca43c2f612de601
Thundorix <dehaan.frederik@gmail.com>
2025-08-08 11:28:43 -0400

    Fixed some typos

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from stats import count_words, sort_dict
 
 
 def main():
-
     if len(sys.argv) != 2:
-        print("Usage: python3 main.py <path_to_book")
+        print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
     book_path = sys.argv[1]
     text = get_book_text(book_path)
